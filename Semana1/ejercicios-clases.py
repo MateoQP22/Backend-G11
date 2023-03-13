a7868324f95cef2e4b6ba5224e8f0c55dc1096f1
MateoQP22 <73276257@continental.edu.pe>
2023-03-12 19:27:19 -0500

    S2-22 Clase ejercicios

--- Semana1/ejercicios-clases.py
+++ Semana1/ejercicios-clases.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 # Crear una clase operaciones y con sus respectivos metodos (sumar, restar, multiplicar, dividir)
 class Operaciones:
     def __init__(self, num1, num2) -> None:
@@ -26,7 +28,42 @@ class Operaciones:
 
     
 x = Operaciones(7, 3)
-print(x.metodoSumar())
-print(x.metodoRestar())
-print(x.metodoDividir())
-print(x.metodoMultiplicar())
+# print(x.metodoSumar())
+# print(x.metodoRestar())
+# print(x.metodoDividir())
+# print(x.metodoMultiplicar())
+
+# Crear una clase de un Usuario que reciba (nombre,edad,dni,estatura,estado_civil) y crear un metodo que nos convierta estos datos en un diccionario
+
+class Usuario:
+    def __init__(self, nombre, edad ,dni ,estatura ,estado_civil) -> None:
+        self.nombre = nombre
+        self.edad = edad
+        self.dni = dni
+        self.estatura = estatura
+        self.estado_civil = estado_civil
+
+    def convertidorDiccionario(self):
+        return {
+            'nombre': self.nombre,
+            'edad':self.edad,
+            'dni':self.dni,
+            'estatura':self.estatura,
+            'estado_civil':self.estado_civil
+        }
+
+        # usuario.append({'nombre':{self.nombre},'edad':{self.edad},'dni':{self.dni},'estatura':{self.estatura},'estado_civil':{self.estado_civil}})
+
+usuario = Usuario('Mateo Quispe Pacheco','25','73276257','1.70','comprometido')
+pprint(usuario.convertidorDiccionario())
+
+
+
+
+
+
+
+
+
+
+
